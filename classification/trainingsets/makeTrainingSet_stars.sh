cf92485c05502086712c387344df9571f3a27fa5
Paul Kuin <npkuin@gmail.com>
2025-02-14 16:26:46 +0000

    training sets used for SUSS 5.0 and TBD scripts to build them automatically for expanded catalogue

---
+++ classification/trainingsets/makeTrainingSet_stars.sh
@@ -0,0 +1,5 @@
+echo
+echo make the training set for stars using the single source catalogue and 
+echo criteria on the proper motion and error
+echo
+echo TBD - use the existing training set for now
---
+++ classification/trainingsets/makeTrainingSets_QSO_galaxies.sh
@@ -0,0 +1,4 @@
+echo 
+echo match SUSS to SDSS to get spectroscopically identified QSOs and galaxies for the training sets
+echo
+echo TBD - use the existing training sets for now 
