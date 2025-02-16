801dd19f2d0afec0c488592a30d9f77987aa0d48
Paul Kuin <npkuin@gmail.com>
2025-02-16 21:03:40 +0000

    this script adds the summary to the main file -- requires astropy

---
+++ tests/add_summary_to_srclist.py
@@ -0,0 +1,9 @@
+#!python
+file1 = "test_cat.fit"
+file2 = "test_cat_summary.fits"
+from astropy.io import fits
+f1 = fits.open(file1)
+f2 = fits.open(file2)
+f1.append(f2[1])
+f1[2].name="SUMMARY"
+f1.writeto(file1,overwrite=True)
