c8d561cf7d06c0b9db19bb61f02660bb62d45e15
Paul Kuin <npkuin@gmail.com>
2025-02-16 21:02:52 +0000

    now also stacks the summary to the extracted test file

--- tests/make_testset.sh
+++ tests/make_testset.sh
@@ -6,5 +6,7 @@ echo
 java -jar /Users/kuin/bin/topcat-full.jar -stilts tpipe   in=$1    ifmt=fits  out=test_cat.fit ofmt=fits  cmd='select (RA>287.80&&RA<287.83)'
 java -jar /Users/kuin/bin/topcat-full.jar -stilts tmatch2 in1=$1"#2" ifmt1=fits in2=test_cat.fit ifmt2=fits out=test_cat_summary.fits matcher=exact+exact values1='N_SUMMARY OBSID' values2='N_SUMMARY OBSID' find=best1 join=all2 fixcols=none
 java -jar /Users/kuin/bin/topcat-full.jar -stilts tpipe in=$1"#2" ifmt=fits out=suss_summary.fit  
-echo rename the test_cat_summary.fits file if needed  
+echo 
+python add_summary_to_srclist.py
+echo
 echo Done
