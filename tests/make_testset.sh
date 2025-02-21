0dd8e9c70065da973ebf310178acb9a328fa5d1a
Paul Kuin <npkuin@gmail.com>
2025-02-21 01:02:25 +0000

    improved version

--- tests/make_testset.sh
+++ tests/make_testset.sh
@@ -7,7 +7,8 @@ date
 java -jar /Users/kuin/bin/topcat-full.jar -stilts tpipe   in=$1  ifmt=fits  out=test_cat.fit ofmt=fits  cmd='select (RA>287.80&&RA<287.83)'
 date
 echo 
-java -jar /Users/kuin/bin/topcat-full.jar -stilts tmatch2 in1=$1"#2" ifmt1=fits in2=test_cat.fit ifmt2=fits out=test_cat_summary.fits icmd2='keepcols "N_SUMMARY OBSID"' matcher=exact+exact values1='N_SUMMARY OBSID' values2='N_SUMMARY OBSID' find=best1 join=all2  ocmd='delcols "N_SUMMARY_2 OBSID_2 Separation"' ocmd='colmeta -name N_SUMMARY "N_SUMMARY_1";colmeta -name OBSID "OBSID_1"'
+#java -jar /Users/kuin/bin/topcat-full.jar -stilts tmatch2 in1=$1"#2" ifmt1=fits in2=test_cat.fit ifmt2=fits out=test_cat_summary.fits icmd2='keepcols "N_SUMMARY OBSID"' matcher=exact+exact values1='N_SUMMARY OBSID' values2='N_SUMMARY OBSID' find=best1 join=all2  ocmd='delcols "N_SUMMARY_2 OBSID_2 Separation"' ocmd='colmeta -name N_SUMMARY "N_SUMMARY_1";colmeta -name OBSID "OBSID_1"'
+java -jar /Users/kuin/bin/topcat-full.jar -stilts tmatch2 in1=$1"#2" ifmt1=fits in2=test_cat.fit ifmt2=fits out=test_cat_summary.fits icmd2='keepcols "N_SUMMARY OBSID"' matcher=exact+exact values1='N_SUMMARY OBSID' values2='N_SUMMARY OBSID' find=best1 join=1and2  ocmd='delcols "N_SUMMARY_2 OBSID_2 Separation"' ocmd='colmeta -name N_SUMMARY "N_SUMMARY_1";colmeta -name OBSID "OBSID_1"'
 date
 echo
 java -jar /Users/kuin/bin/topcat-full.jar -stilts tpipe in=$1"#2" ifmt=fits out=suss_summary.fits  
